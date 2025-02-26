1dbe083f8d25b450a1eb3b93a12606552043f672
Arnab <arnabpramanik102018@gmail.com>
2025-02-26 23:40:50 +0530

    jwt error

--- backend/controlers/generateToken.js
+++ backend/controlers/generateToken.js
@@ -6,9 +6,11 @@ const secret_key = process.env.JWT_SECRET;
 
 const generateToken = async (id) => {
     try {
+        console.log(secret_key)
         const token = jwt.sign({ id }, secret_key, {
             expiresIn: "30d"  // Set token expiration
         });
+    
         return token;  // Return the generated token
     } catch (error) {
         console.error("Error generating token:", error);  // Log any errors
